834594584d964b03703bbd1cbf6977084204be13
ilove028 <maoxuheng@cnpc.com.cn>
2020-12-02 11:04:58 +0800

    chore: use d3 and webgl complete line

--- fundamentals/js/index.js
+++ fundamentals/js/index.js
@@ -13,9 +13,10 @@
     .domain(0, canvas.height)
     .range([-1, 1]);
   gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0, 0, 0.5, 0, 0, 0.5]), gl.STATIC_DRAW);
+  const vertices = new Float32Array([0, 0, 0.5, 0, 0, 0.5]);
+  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
   const { width, height } = canvas;
-  gl.viewport(width / 4, 0, width, height);
+  // gl.viewport(width / 4, 0, width, height);
   gl.clearColor(0, 0, 0, 0);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
@@ -23,7 +24,7 @@
 
   gl.enableVertexAttribArray(aPosition);
 
-  gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+  gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, vertices.BYTES_PER_ELEMENT * 2, 0);
 
   gl.drawArrays(gl.TRIANGLES, 0, 3);
 })();

18c27de10048c547d3ae50c75e69e2f8f8587ba7
mxilove028 <892447525@qq.com>
2020-12-29 22:17:00 +0800

    feat: draw triangle

--- chapter1/js/index.js
+++ chapter1/js/index.js
@@ -24,10 +24,15 @@
   const vPosition = gl.getAttribLocation(program, 'aPosition');
   const aColor = gl.getAttribLocation(program, 'aColor');
   const vertices = [
-    1.0,  0,   0, 1, 1, 0,
-    0,    1.0, 0, 0, 1, 1,
-    -1.0, 0,   0, 1, 0, 1
+    1.0,  0,    0,
+    0,    1.0,  0,
+    -1.0, 0,    0,
+    0,    -1.0, 0
   ];
+  const indices = [
+    0, 1, 2,
+    0, 2, 3
+  ]
 
   const buffer = gl.createBuffer();
   const data = new Float32Array(vertices)
@@ -35,15 +40,53 @@
   gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
 
   gl.enableVertexAttribArray(vPosition);
-  gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, data.BYTES_PER_ELEMENT * 6, 0);
+  gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 3 * data.BYTES_PER_ELEMENT, 0);
 
-  gl.enableVertexAttribArray(aColor);
-  gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, data.BYTES_PER_ELEMENT * 6, data.BYTES_PER_ELEMENT * 3);
+  gl.vertexAttrib4f(aColor, 1, 1, 0, 1);
+  // gl.enableVertexAttribArray(aColor);
+  // gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, data.BYTES_PER_ELEMENT * 6, data.BYTES_PER_ELEMENT * 3);
+
+  const indexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
 
   gl.clearColor(0, 0, 0, 1);
+  
+  // gl.drawArrays(gl.TRIANGLES, 0, 3);
   gl.clear(gl.COLOR_BUFFER_BIT);
   gl.useProgram(program);
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
+  // 使用索引缓冲区 顶点缓冲区index由索引决定 可以任意顺序，而直接使用drawArrays index只能0 1 2， 3增加。
+  gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+  /**
+   * getBydIndex(index) {
+   *  getFromArrayBuffer(index)
+   * }
+   * 
+   * getFromArrayBuffer(index) {
+   *  const start = index * stride * BYTES_PER_ELEMENT + offset * BYTES_PER_ELEMENT
+   *  const end = start + size * FLOAT.BYTES_PER_FLOAT;
+   * }
+   * 
+   * setAttribute(name, value) {
+   *   name = value
+   * }
+   * 
+   * vertexShaderFunction(index, count isIndex) {
+   *  const attributes = findAttributes();
+   *  if (isIndex) {
+   *    const i = indexBuffer[index]
+   *    attributes.forEach(a => setAttribute(a, getFromArrayBuffer(i)));
+   *  } else {
+   *    attributes.forEach(a => setAttribute(a, getFromArrayBuffer(index)));
+   *  }
+   * }
+   * 
+   * for (let i = 0; i < 6) {
+   *  {
+   *    vertexShaderFunction(i, count = 6, true);
+   *  }
+   * }
+   */
 })({
   createShader(context, type, source) {
     const shader = context.createShader(type);
---
+++ chapter2/js/index.js
@@ -0,0 +1,42 @@
+(function() {
+  const canvas = document.querySelector('#canvas');
+  const gl = canvas.getContext('webgl');
+  const VS_SOURCE = `
+    attribute vec4 aPosition;
+    void main() {
+      gl_Position = aPosition;
+    }
+  `;
+  const FS_SOURCE = `
+    precision mediump float;
+    uniform vec4 uColor;
+    void main() {
+      gl_FragColor = uColor;
+    }
+  `;
+  const vertexShader = Util.createShader(gl, gl.VERTEX_SHADER, VS_SOURCE);
+  const fragShader = Util.createShader(gl, gl.FRAGMENT_SHADER, FS_SOURCE);
+  const program = Util.createProgram(gl, vertexShader, fragShader);
+
+  // Init buffer
+  const vertices = [
+    0, 0,
+    0, 1,
+    1, 0
+  ];
+  const data = new Float32Array(vertices)
+  const buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+
+  // draw
+  const aPosition = gl.getAttribLocation(program, 'aPosition');
+  const uColor = gl.getUniformLocation(program, 'uColor');
+  gl.clearColor(0, 0, 0, 1);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.enableVertexAttribArray(aPosition);
+  gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 2 * data.BYTES_PER_ELEMENT, 0);
+  gl.useProgram(program);
+  gl.uniform4f(uColor, 1, 1, 0, 1);
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
+})({})

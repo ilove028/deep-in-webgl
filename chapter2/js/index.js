df1ce2064c0174922a690418e45ef3f01d8e3a4e
mxilove028 <892447525@qq.com>
2020-12-29 22:50:49 +0800

    chore: animate circle

--- chapter2/js/index.js
+++ chapter2/js/index.js
@@ -20,23 +20,38 @@
 
   // Init buffer
   const vertices = [
-    0, 0,
-    0, 1,
-    1, 0
+    0, 0
   ];
   const data = new Float32Array(vertices)
   const buffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
   gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
 
-  // draw
-  const aPosition = gl.getAttribLocation(program, 'aPosition');
-  const uColor = gl.getUniformLocation(program, 'uColor');
-  gl.clearColor(0, 0, 0, 1);
-  gl.clear(gl.COLOR_BUFFER_BIT);
-  gl.enableVertexAttribArray(aPosition);
-  gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 2 * data.BYTES_PER_ELEMENT, 0);
-  gl.useProgram(program);
-  gl.uniform4f(uColor, 1, 1, 0, 1);
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
+  function fillBuffer(now) {
+    const raidus = now / 500 * Math.PI;
+    vertices.push(Math.cos(raidus), Math.sin(raidus));
+    const data = new Float32Array(vertices);
+    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+  }
+
+  function render(now) {
+    // draw
+    const aPosition = gl.getAttribLocation(program, 'aPosition');
+    const uColor = gl.getUniformLocation(program, 'uColor');
+    gl.clearColor(0, 0, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.enableVertexAttribArray(aPosition);
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 2 * data.BYTES_PER_ELEMENT, 0);
+    gl.useProgram(program);
+    gl.uniform4f(uColor, 1, 1, 0, 1);
+    fillBuffer(now);
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, vertices.length / 2);
+  }
+
+  function tick(now) {
+    render(now);
+    requestAnimationFrame(tick);
+  }
+
+  tick(0)
 })({})

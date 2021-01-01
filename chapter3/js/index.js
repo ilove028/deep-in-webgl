f35eb854e4c04f0451103b4390c3a57dda0db1cc
mxilove028 <892447525@qq.com>
2021-01-01 10:48:24 +0800

    chore: add move feat

--- chapter3/js/index.js
+++ chapter3/js/index.js
@@ -5,10 +5,11 @@
     attribute vec2 aPosition;
     attribute vec2 aUv;
     uniform mat4 uProject;
+    uniform float uX;
     varying vec2 vUv;
     void main() {
       gl_Position = uProject * vec4(aPosition, 0.0, 1.0);
-      vUv = aUv;
+      vUv = vec2(aUv.x, aUv.y + uX);
     }
   `;
   const FS_SOURCE = `
@@ -27,8 +28,9 @@
   // texture
   const texture = gl.createTexture();
   const img = new Image();
-  img.src = 'https://webglfundamentals.org/webgl/resources/leaves.jpg';
+  img.src = 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fningbo.sinaimg.cn%2F2014%2F0424%2FU7974P1480DT20140424141334.png&refer=http%3A%2F%2Fningbo.sinaimg.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1612060092&t=a6e0291e7d870a1a83feb895eac02543';
   img.crossOrigin = 'anonymous';
+  // img.src = 'bg.png';
   texture.img = img;
   img.onload = function() {
     render(texture)
@@ -51,28 +53,41 @@
 
     const aPosition = gl.getAttribLocation(program, 'aPosition');
     const aUv = gl.getAttribLocation(program, 'aUv');
+    const uX = gl.getUniformLocation(program, 'uX');
     
     gl.bindTexture(gl.TEXTURE_2D, texture);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.img);
 
     const uProject = gl.getUniformLocation(program, 'uProject');
 
-    gl.clearColor(0, 0, 0, 0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    // gl.clearColor(0, 0, 0, 0);
+    // gl.clear(gl.COLOR_BUFFER_BIT);
 
     gl.useProgram(program);
-
+    let alpha = 0;
     gl.uniformMatrix4fv(uProject, false, proMatrix);
+    gl.uniform1f(uX, alpha);
 
     gl.enableVertexAttribArray(aPosition);
     gl.enableVertexAttribArray(aUv);
 
     gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 4 * data.BYTES_PER_ELEMENT, 0);
     gl.vertexAttribPointer(aUv, 2, gl.FLOAT, false, 4 * data.BYTES_PER_ELEMENT, 2 * data.BYTES_PER_ELEMENT);
-    gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+    function tick() {
+      animate();
+      requestAnimationFrame(tick)
+    }
+    function animate() {
+      gl.uniform1f(uX, alpha);
+      gl.drawArrays(gl.TRIANGLES, 0, 6);
+      alpha += 0.01;
+    }
+
+    tick()
   }
 })()
